a305f331bbfd4c691ee911cc9d53a12f2cad81ce
Kaique da Silva <kaique.developer@gmail.com>
2012-06-10 23:10:18 -0300

    Node.js Web Crawler Codename Metabee

---
+++ metabee-test.js
@@ -0,0 +1,9 @@
+var vows = require('vows'),
+    assert = require('assert'),
+    Metabee = require('./metabee').activate;
+
+vows.describe('Metabee web crawler tests').addBatch({
+    'Access layer': {
+        // Access layter tests
+    }
+}).run();
---
+++ metabee.js
@@ -0,0 +1,14 @@
+var Metabee = (function () {
+    var _ = function () {};
+
+    // access the url and get content
+    _.prototype.connect = function ( settings ) {
+        var output = {};
+
+        return output;
+    };
+
+    return new ( _ );
+} ());
+
+exports.activate = Metabee;
